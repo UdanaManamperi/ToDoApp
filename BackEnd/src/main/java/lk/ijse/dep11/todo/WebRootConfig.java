70eff833d5da06a0c66c16994585da12a1fa7db3
udana <udanaoneplus@gmail.com>
2024-01-25 20:06:05 +0530

    Establish connection pool with database using HikariCP

--- BackEnd/src/main/java/lk/ijse/dep11/todo/WebRootConfig.java
+++ BackEnd/src/main/java/lk/ijse/dep11/todo/WebRootConfig.java
@@ -1,5 +1,30 @@
 package lk.ijse.dep11.todo;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
 public class WebRootConfig {
+
+    @Bean(destroyMethod = "close")
+    public HikariDataSource dataSource(Environment env){
+
+        HikariConfig config = new HikariConfig();
+
+        config.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size", Integer.class));
+                return  new HikariDataSource(config);
+
+
+
+    }
 
 }
