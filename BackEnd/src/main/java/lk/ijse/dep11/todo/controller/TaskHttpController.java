67678b318c5e71b7addbae6f124ffffd9078b307
udana <udanaoneplus@gmail.com>
2024-01-25 15:22:42 +0530

    Add requst handler methods to TaskHttpController class

--- BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
+++ BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
@@ -7,4 +7,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/tasks")
 @CrossOrigin
 public class TaskHttpController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createTask() {
+        System.out.println("createTask()");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping
+    public void updateTask() {
+        System.out.println("updateTask");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping
+    public void deleteTask() {
+        System.out.println("deleteTask");
+    }
+
+    @GetMapping
+    public void getAllTasks() {
+        System.out.println("getAllTasks");
+    }
+
+
 }
