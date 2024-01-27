9384fc3ffc214593457da6f1f6de6ae4945acaed
udana <udanaoneplus@gmail.com>
2024-01-27 16:08:15 +0530

    Complete request handler methods with business logics

--- BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
+++ BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
@@ -1,34 +1,113 @@
 package lk.ijse.dep11.todo.controller;
 
+import lk.ijse.dep11.todo.to.TaskTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/tasks")
 @CrossOrigin
 public class TaskHttpController {
+    @Autowired
+    private DataSource pool;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void createTask() {
-        System.out.println("createTask()");
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public TaskTO createTask(@RequestBody @Validated TaskTO task) {
+        try {
+            Connection connection = pool.getConnection();
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO task (description, status, email) VALUES (?,FALSE,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1,task.getDescription());
+            stm.setString(2, task.getEmail());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            task.setId(id);
+            task.setStatus(false);
+            return task;
+
+        }catch (SQLException e){
+
+            throw new RuntimeException(e);
+
+    }
+
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping
-    public void updateTask() {
-        System.out.println("updateTask");
+    @PatchMapping(value = "/{id}",consumes = "application/json")
+    public void updateTask(@PathVariable int id,@RequestBody @Validated(TaskTO.Update.class)TaskTO task) {
+       try {
+           Connection connection = pool.getConnection();
+           PreparedStatement stmExist = connection.prepareStatement("SELECT 8 FROM task WHERE id=?");
+           stmExist.setInt(1,id);
+           if(!stmExist.executeQuery().next()){
+               throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Task Not Found");
+           }
+           PreparedStatement stm = connection.prepareStatement("UPDATE task SET description=?,status=? WHERE id=?");
+            stm.setString(1, task.getDescription());
+            stm.setBoolean(2, task.getStatus());
+            stm.setInt(3,id);
+            stm.executeUpdate();
+
+       }catch (SQLException e){
+           throw  new RuntimeException(e);
+
+       }
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping
-    public void deleteTask() {
-        System.out.println("deleteTask");
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable ("id") int taskId) {
+        try {
+            Connection connection = pool.getConnection();
+            PreparedStatement stmExist = connection.prepareStatement("SELECT * FROM task WHERE id=?");
+            stmExist.setInt(1,taskId);
+            if(!stmExist.executeQuery().next()){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Task not found");
+            }
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM  task WHERE id=?");
+            stm.setInt(1,taskId);
+            stm.executeUpdate();
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
     }
 
-    @GetMapping
-    public void getAllTasks() {
-        System.out.println("getAllTasks");
+    @GetMapping(produces = "application/json",params = {"email"})
+    public List<TaskTO>  getAllTasks(String email) {
+       try {
+           Connection connection = pool.getConnection();
+           PreparedStatement stm = connection.prepareStatement("SELECT * FROM  task WHERE email=?");
+           stm.setString(1,email);
+           ResultSet rst = stm.executeQuery();
+           LinkedList<TaskTO> taskList = new LinkedList<>();
+           while(rst.next()){
+               int id = rst.getInt("id");
+               String description = rst.getString("description");
+
+               boolean status = rst.getBoolean("status");
+               taskList.add(new TaskTO(id,description,status,email));
+
+           }
+           return  taskList;
+
+
+       }catch (SQLException e){
+           throw new RuntimeException(e);
+       }
     }
 
 
--- BackEnd/src/main/java/lk/ijse/dep11/todo/to/TaskTO.java
+++ BackEnd/src/main/java/lk/ijse/dep11/todo/to/TaskTO.java
@@ -1,24 +1,30 @@
 package lk.ijse.dep11.todo.to;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.*;
+import javax.validation.groups.Default;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskTO implements Serializable {
-    @Null(message = "Id should be empty")
+
     private Integer id;
     @NotBlank(message = "Description should not be empty")
     private String description;
-    @NotNull(message = "Status should not be empty")
+    @NotNull(message = "Status should not be empty", groups = Update.class)
+    @Null(message = "Status should be empty", groups = Create.class)
     private Boolean status;
     @NotEmpty(message = "Email can't be empty")
     @Email
     private String email;
+
+    public interface Update extends Builder.Default{}
+    public interface Create extends Builder.Default{}
 }
