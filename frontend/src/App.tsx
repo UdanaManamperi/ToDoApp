5cf81369ea2ca854fbdd8e06f980a2715258be60
udana <udanaoneplus@gmail.com>
2024-01-29 03:30:30 +0530

    Add loader component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,24 @@
 import './App.css'
 import {SignIn} from "./component/signin/SignIn.tsx";
 import {useUser, useUserDispatcher} from "./context/UserContext.tsx";
-import {useEffect} from "react";
+import {useEffect,useState} from "react";
 import {onAuthStateChanged} from 'firebase/auth'
 import {auth} from "./firebase.ts";
+import {Loader} from "./component/loader/Loader.tsx"
 
 
 
 function App() {
     const user = useUser();
     const userDispatcher = useUserDispatcher();
+    const [loader,setLoader]=useState(true);
+
+
 
     useEffect(() => {
         onAuthStateChanged(auth, user => {
             if(user) {
+                setLoader(false);
                 userDispatcher({type: 'sign-in', user})
             } else {
                 userDispatcher({type: 'sign-out'})
@@ -25,7 +30,10 @@ function App() {
 
   return (
     <>
-        {user? <h1>To-do App</h1>: <SignIn/>}
+        {loader? <Loader/> :
+            user?
+                ( <h1>To do app</h1>): <SignIn/>
+        }
     </>
   )
 }
---
+++ frontend/src/component/loader/Loader.tsx
@@ -0,0 +1,14 @@
+import './Loader.css'
+
+export function Loader() {
+    return (
+        <div className="Loader d-flex vh-100 justify-content-center align-items-center">
+            <div className="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
