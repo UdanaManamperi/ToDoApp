66fb5f122508918493ed7c96d1569485a532b30e
udana <udanaoneplus@gmail.com>
2024-01-29 03:22:50 +0530

    Complete sign-in page with Google sign-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 
 import './App.css'
 import {SignIn} from "./component/signin/SignIn.tsx";
+import {useUser, useUserDispatcher} from "./context/UserContext.tsx";
+import {useEffect} from "react";
+import {onAuthStateChanged} from 'firebase/auth'
+import {auth} from "./firebase.ts";
+
+
 
 function App() {
+    const user = useUser();
+    const userDispatcher = useUserDispatcher();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if(user) {
+                userDispatcher({type: 'sign-in', user})
+            } else {
+                userDispatcher({type: 'sign-out'})
+            }
+        })
+    }, []);
 
 
   return (
     <>
-      <SignIn/>
+        {user? <h1>To-do App</h1>: <SignIn/>}
     </>
   )
 }
